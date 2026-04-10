f4a0f566a1c320a597193c9ae7bc80b1f5e53602
DiogoH9 <dhpo@aluno.ifnmg.edu.br>
2026-04-10 02:37:55 +0000

    feat: decima e tentativa final de fazer esse codigo funcionar

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -7,25 +7,53 @@
 
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
-  // escreva seu código aqui
+  if (idade < 12) {
+    return "criança";
+  } else if (idade <= 17) {
+    return "adolescente";
+  } else {
+    return "adulto";
+  }
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  // escreva seu código aqui
+  return preco - (preco * percentual / 100);
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  // escreva seu código aqui
+  if (nota >= 60) {
+    return "aprovado";
+  } else if (nota >= 40) {
+    return "recupera\u00E7\u00E3o";
+  } else {
+    return "reprovado";
+  }
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  // escreva seu código aqui
+  let soma = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+      soma += numeros[i];
+    }
+  }
+
+  return soma;
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  // escreva seu código aqui
+  let contador = 0;
+
+  for (let i = 0; i < texto.length; i++) {
+    if (texto[i] === letra) {
+      contador++;
+    }
+  }
+
+  return contador;
 }
